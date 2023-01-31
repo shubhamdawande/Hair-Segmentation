d8e0874efe5d4995cadc1b672da2b58fcc6bce33
shubham dawande <shubham@perceptimagery.com>
2023-02-01 01:55:06 +0530

    switched to improved mobile unet with better inference speed

--- demo-app/image-prediction/index.js
+++ demo-app/image-prediction/index.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+img_size = 256
 
 const loadModel = async () => {
   // const model = await tflite.loadTFLiteModel('./models/hair_segmentation.tflite'); // lite model not work yet
-  const model = await tf.loadLayersModel('../models/best_model_1.json');
+  const model = await tf.loadLayersModel('../models/prism/model.json');
 
   const startTime = performance.now();
   const outputTensor = tf.tidy(() => {
@@ -15,18 +16,16 @@ const loadModel = async () => {
     console.log('Prediction time: ', performance.now() - startTime);
 
     // 2nd prediction onwards takes < 10ms
-    // const img2 = tf.browser.fromPixels(document.getElementById('sample2'));
-    // const input2 = img2.expandDims().div(127.5).sub(1);
-    // const startTime2 = performance.now();
-    // let outputTensor2 = model.predict(input2);
-    // console.log('Prediction time 2: ', performance.now() - startTime2);
+    const startTime2 = performance.now();
+    let outputTensor2 = model.predict(input);
+    console.log('Prediction time 2: ', performance.now() - startTime2);
 
     return outputTensor.round().add(1).mul(127.5).squeeze();
   });
   const data = Array.from(outputTensor.dataSync())
   console.log('Prediction time: ', performance.now() - startTime);
-  const rgbaImage = Array.from(new Float32Array(320*320*4));
-  for(let i = 0, j = 0; i < 320 * 320 * 4, j < 320 * 320; i += 4, j += 1){
+  const rgbaImage = Array.from(new Float32Array(img_size*img_size*4));
+  for(let i = 0, j = 0; i < img_size * img_size * 4, j < img_size * img_size; i += 4, j += 1){
     if(data[j] === 255){
       rgbaImage[i] = 0;
       rgbaImage[i + 1] = 255;
@@ -38,12 +37,12 @@ const loadModel = async () => {
     }
     rgbaImage[i + 3] = 50;
   }
-  const imageData = new ImageData(Uint8ClampedArray.from(rgbaImage), 320, 320);
+  const imageData = new ImageData(Uint8ClampedArray.from(rgbaImage), img_size, img_size);
   ctx.putImageData(imageData, 0, 0);
 
   const originalImg = document.getElementById('sample');
   ctx.globalAlpha = 0.7;
-  ctx.drawImage(originalImg, 0, 0, 320, 320);
+  ctx.drawImage(originalImg, 0, 0, img_size, img_size);
 }
 
 loadModel();
--- demo-app/webcam-prediction/index.js
+++ demo-app/webcam-prediction/index.js
@@ -5,9 +5,10 @@ const ctx1 = canvas1.getContext('2d');
 const ctx2 = canvas2.getContext('2d');
 let model = null;
 // tf.setBackend('wasm');
+img_size = 256
 
 const loadModel = async () => {
-    model = await tf.loadLayersModel('../models/best_model_1.json');
+    model = await tf.loadLayersModel('../models/prism/model.json');
 }
 
 const startWebcam = async () => {
@@ -25,29 +26,35 @@ const predictWebcam = async () => {
 
 const drawCanvas = () => {
     // draw video to canvas1
-    canvas1.height = 320; // video.videoHeight / 2;
-    canvas1.width = 320; // video.videoWidth / 2;
+    canvas1.height = img_size; // video.videoHeight / 2;
+    canvas1.width = img_size; // video.videoWidth / 2;
     ctx1.drawImage(video, 0, 0, canvas1.width, canvas1.height);
 
     // get image from canvas1    
-    const img = ctx1.getImageData(0, 0, 320, 320);
+    const img = ctx1.getImageData(0, 0, img_size, img_size);
     predictImage(img);
     window.requestAnimationFrame(drawCanvas)
 }
 
 // element: htmlImage / htmlVideo / ImageData
 const predictImage = async (element) => {
+    const startTime = performance.now();
     const outputTensor = tf.tidy(() => {
         const img = tf.browser.fromPixels(element);
-        const input = img.expandDims().div(127.5).sub(1);
+        // const input = img.expandDims().div(127.5).sub(1);
+        const input = img.expandDims().div(255);
+         
         let outputTensor = model.predict(input);
-        outputTensor = outputTensor.round().add(1).mul(127.5).squeeze();
+
+        // outputTensor = outputTensor.round().add(1).mul(127.5).squeeze();
+        outputTensor = outputTensor.round().mul(255).squeeze();
         return outputTensor
     });
     const data = outputTensor.dataSync();
+    console.log('prediction time: ', performance.now() - startTime);
     const dataArr = Array.from(data);
-    const rgbaImage = Array.from(new Float32Array(320*320*4));
-    for(let i = 0, j = 0; i < 320 * 320 * 4, j < 320 * 320; i += 4, j += 1){
+    const rgbaImage = Array.from(new Float32Array(img_size*img_size*4));
+    for(let i = 0, j = 0; i < img_size * img_size * 4, j < img_size * img_size; i += 4, j += 1){
         if(dataArr[j] === 255){
         rgbaImage[i] = 0;
         rgbaImage[i + 1] = 255;
@@ -59,12 +66,12 @@ const predictImage = async (element) => {
         }
         rgbaImage[i + 3] = 100;
     }
-    const imageData = new ImageData(Uint8ClampedArray.from(rgbaImage), 320, 320);
+    const imageData = new ImageData(Uint8ClampedArray.from(rgbaImage), img_size, img_size);
     ctx2.putImageData(imageData, 0, 0);
     const canvasOriginalImg = new Image();
     canvasOriginalImg.src = element;
     ctx2.globalAlpha = 0.9;
-    ctx2.drawImage(canvasOriginalImg, 0, 0, 320, 320);
+    ctx2.drawImage(canvasOriginalImg, 0, 0, img_size, img_size);
 }
 
 startWebcam()
